257b0481484cee34986a86e93eefee1348db8c38
Markus Suomalainen <markus.suomalainen@gmail.com>
2017-04-09 16:43:09 +0300

    Deleted useless import

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,6 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
